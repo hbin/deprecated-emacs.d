cac830c63ba10589e093b0f11ee03ee686b6e3fd
Huang Bin <embrace.hbin@gmail.com>
2012-05-01 10:47:46 +0800

    IMPROVE: add desktop settings.

--- hbin/misc-hist.el
+++ hbin/misc-hist.el
@@ -24,32 +24,100 @@
 
 ;;; Code:
 
-;; Auto Complete cache
-(setq ac-comphist-file (concat tmp-dir "ac.cache"))
-
-;; Ido cache
-(setq ido-save-directory-list-file (concat tmp-dir "ido.cache"))
-
-;; 保存书签历史
-(setq bookmark-default-file (concat tmp-dir "bookmarks.cache"))
-
-;; 保存游标在访问过文件中的位置
-(setq save-place-file (concat tmp-dir "saveplace.cache"))
-(setq-default save-place t)
-(require 'saveplace)
-
-;; savehist: save some history
-(setq savehist-additional-variables
-      '(search ring regexp-search-ring)
-      savehist-autosave-interval 60
-      savehist-file (concat tmp-dir "savehist.cache"))
-(savehist-mode t)
-
 ;; 将备份文件和自动保存临时文件放到临时文件夹
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; Save some history/cache
+(setq ac-comphist-file (concat tmp-dir "ac.cache"))                 ; Auto Complete history
+(setq ido-save-directory-list-file (concat tmp-dir "ido.cache"))    ; Ido history
+(setq bookmark-default-file (concat tmp-dir "bookmarks.cache"))     ; Bookmark history
+(setq save-place-file (concat tmp-dir "saveplace.cache"))
+(setq savehist-file (concat tmp-dir "savehist.cache"))
+
+;; Enable history-save
+(savehist-mode t)
+(setq-default save-place t)
+(require 'saveplace)
+
+;; Desktop
+(require 'desktop)
+
+;; save the desktop file automatically if it already exists
+(desktop-save-mode 1)
+
+;; use only one desktop
+(setq desktop-path '("~/.emacs.d/tmp/"))
+(setq desktop-dirname "~/.emacs.d/tmp/")
+(setq desktop-base-file-name "desktop.cache")
+
+(setq desktop-restore-eager 15)
+
+;; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((extended-command-history . 30)
+                (file-name-history        . 100)
+                (grep-history             . 30)
+                (compile-history          . 30)
+                (minibuffer-history       . 50)
+                (query-replace-history    . 60)
+                (read-expression-history  . 60)
+                (regexp-history           . 60)
+                (regexp-search-ring       . 20)
+                (search-ring              . 20)
+                (shell-command-history    . 50)
+                tags-file-name
+                register-alist)))
+
+;; specify buffers which should not be saved, by name or by mode
+(setq desktop-buffers-not-to-save
+      (concat "\\("
+              "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
+              "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.bbdb"
+              "\\)$"))
+(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+(add-to-list 'desktop-modes-not-to-save 'helm-mode)
+(add-to-list 'desktop-modes-not-to-save 'Info-mode)
+(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+(add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
+
+;; remove desktop after it's been read
+(add-hook 'desktop-after-read-hook
+          '(lambda ()
+             (setq desktop-dirname-tmp desktop-dirname)
+             (desktop-remove)
+             (setq desktop-dirname desktop-dirname-tmp)))
+
+(defun saved-session ()
+  (file-exists-p (concat desktop-dirname "/" desktop-base-file-name)))
+
+;; use session-restore to restore the desktop manually
+(defun session-restore ()
+  "Restore a saved emacs session."
+  (interactive)
+  (if (saved-session)
+      (desktop-read)
+    (message "No desktop found.")))
+
+;; use session-save to save the desktop manually
+(defun session-save ()
+  "Save an emacs session."
+  (interactive)
+  (if (saved-session)
+      (if (y-or-n-p "Overwrite existing desktop? ")
+          (desktop-save-in-desktop-dir)
+        (message "Session not saved."))
+    (desktop-save-in-desktop-dir)))
+
+;; ask user whether to restore desktop at start-up
+(add-hook 'after-init-hook
+          '(lambda ()
+             (if (saved-session)
+                 (if (not (y-or-n-p "Restore desktop? "))
+                     (session-restore)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
