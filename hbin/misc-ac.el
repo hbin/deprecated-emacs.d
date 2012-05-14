e5d852b9722f29f5b00ae3e4b81c8747daccd283
hbin <embrace.hbin@gmail.com>
2012-05-15 00:31:03 +0800

    FIX: remove yasnippet source.

--- hbin/misc-ac.el
+++ hbin/misc-ac.el
@@ -59,8 +59,7 @@
                              ac-source-symbols) ac-sources)))
 
 (defun ac-cc-mode-setup ()
-  (setq ac-sources (append '(ac-source-yasnippet
-                             ac-source-gtags) ac-sources)))
+  (setq ac-sources (append '(ac-source-gtags) ac-sources)))
 
 (defun ac-ruby-mode-setup ()
   (setq ac-sources (append '(ac-source-rsense
