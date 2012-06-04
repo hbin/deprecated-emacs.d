38bd51afe7e15a304047d6fbbc5275df25bd6b4b
hbin <embrace.hbin@gmail.com>
2012-06-04 17:47:02 +0800

    IMPR: Add ac dictionaries, and fix compatibility problem with yasnippet-0.7.0.

--- hbin/misc-ac.el
+++ hbin/misc-ac.el
@@ -30,6 +30,10 @@
 
 (require 'auto-complete-config)
 
+;; ac-dictionaries for major modes
+(add-to-list 'ac-dictionary-directories (concat user-emacs-directory "ac-dict"))
+
+;; ac common settings
 (setq ac-use-quick-help nil)                            ; 不需要 quick-help
 (setq ac-auto-show-menu nil)                            ; 取消默认弹出补全菜单
 (setq ac-menu-height 15)                                ; ....补全菜单长一点
@@ -48,6 +52,24 @@
 (setq ac-stop-flymake-on-completing t)
 (ac-flyspell-workaround)
 
+;; Override this function to fix compatibility issue with yasnippet-0.7.0.
+(defun ac-yasnippet-candidates ()
+  (with-no-warnings
+    (if (fboundp 'yas/get-snippet-tables)
+        (if (null (help-function-arglist 'yas/get-snippet-tables))
+            ;; >0.7.0
+            (apply 'append (mapcar 'ac-yasnippet-candidate-1 (yas/get-snippet-tables)))
+          ;; 0.6.0 < x < 0.7.0
+          (apply 'append (mapcar 'ac-yasnippet-candidate-1 (yas/get-snippet-tables major-mode))))
+      (let ((table
+             (if (fboundp 'yas/snippet-table)
+                 ;; <0.6.0
+                 (yas/snippet-table major-mode)
+               ;; 0.6.0
+               (yas/current-snippet-table))))
+        (if table
+            (ac-yasnippet-candidate-1 table))))))
+
 ;; Override the default settings
 (defun ac-common-setup ()
   (add-to-list 'ac-sources 'ac-source-filename))
@@ -62,14 +84,13 @@
   (setq ac-sources (append '(ac-source-gtags) ac-sources)))
 
 (defun ac-ruby-mode-setup ()
-  (setq ac-sources (append '(ac-source-rsense
-                             ac-source-gtags) ac-sources)))
+  (setq ac-sources (append '(ac-source-gtags) ac-sources)))
 
 (defun ac-css-mode-setup ()
   (setq ac-sources (append '(ac-source-css-property) ac-sources)))
 
 (defun ac-config-default ()
-  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (setq-default ac-sources '(ac-source-yasnippet ac-source-dictionary ac-source-words-in-same-mode-buffers))
   (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
   (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
