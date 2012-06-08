faf66e4b71cee7de2623227c406506ccd8e33f9f
hbin <embrace.hbin@gmail.com>
2012-06-08 22:35:52 +0800

    FIX: .rjs .rxml documents are ruby code too.

--- hbin/prog-rails.el
+++ hbin/prog-rails.el
@@ -41,6 +41,8 @@
 (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rjs$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rxml$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
