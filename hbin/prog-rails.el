b6d10fe09def51038d4e329383a7e7370fc2e863
hbin <embrace.hbin@gmail.com>
2012-08-13 21:05:01 +0800

    FIX: Guardfile is ruby script.

--- hbin/prog-rails.el
+++ hbin/prog-rails.el
@@ -44,8 +44,8 @@
 (add-to-list 'auto-mode-alist '("\\.rjs$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rxml$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
