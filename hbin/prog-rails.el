47434d6ce3e2244689f644233f0a1001389fd894
hbin <embrace.hbin@gmail.com>
2012-08-14 20:42:56 +0800

    OPTIMIZE: Add the scss-mode and remove the never used nxhtml-mode.

--- hbin/prog-rails.el
+++ hbin/prog-rails.el
@@ -24,15 +24,6 @@
 
 ;;; Code:
 
-(autoload 'yaml-mode "yaml-mode" "Edit yml document" t)
-(autoload 'haml-mode "haml-mode" "Edit haml document" t)
-(autoload 'slim-mode "slim-mode" "Edit slim documetn" t)
-(autoload 'sass-mode "sass-mode" "Edit sass documetn" t)
-(autoload 'rspec-mode "rspec-mode" "RSpec test mode" t)
-(autoload 'rhtml-mode "rhtml-mode" "Edit erb document" t)
-(autoload 'eruby-nxhtml-mumamo-mode "autostart.el" "Edit erb document" t)
-(autoload 'coffee-mode "coffee-mode" "Display color names with colored background" t)
-
 ;; We never want to edit Rubinius bytecode or MacRuby binaries
 (add-to-list 'completion-ignored-extensions ".rbc")
 (add-to-list 'completion-ignored-extensions ".rbo")
@@ -50,43 +41,26 @@
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
-;; Edit yml documents
-(add-to-list 'auto-mode-alist '("\\.yml" . yaml-mode))
-
-;; Edit erb documents
-(add-to-list 'auto-mode-alist '("\\.erb" . rhtml-mode))
-
-;; Edit haml documents
-(add-to-list 'auto-mode-alist '("\\.haml" . haml-mode))
-
-;; Edit slim documents
-(add-to-list 'auto-mode-alist '("\\.slim" . slim-mode))
-
-;; Edit sass documents
-(add-to-list 'auto-mode-alist '("\\.sass" . sass-mode))
-
-;; Edit coffee script
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-;; It's an amazing mode for editing erb, jsp..etc documents.
-;; But I got an error in menu-bar-update-hook occasionally. Error Message:
-;; (error "This buffer cannot use 'imenu-default-create-index-function'")
-;; (add-to-list 'auto-mode-alist '("\\.erb" . eruby-nxhtml-mumamo-mode))
-
 ;;;###autoload
 (progn
-  (require 'rinari)
+  (require 'haml-mode)
+  (require 'sass-mode)
+  (require 'slim-mode)
+  (require 'yaml-mode)
 
-  ;; Automatically updating TAGS
-  (setq rinari-tags-file-name "TAGS")
+  (require 'coffee-mode)
+  (custom-set-variables '(coffee-tab-width 2))
 
-  ;; disable mumamo color
-  (add-hook 'nxhtml-mode-hook
+  ;; Setting Scss
+  (require 'scss-mode)
+  (add-hook 'scss-mode-hook
             (lambda ()
-              (setq mumamo-background-colors nil)))
+              (rainbow-mode)
+              (setq scss-compile-at-save nil)
+              (define-key scss-mode-map (kbd "C-c C-c") 'whole-line-or-region-comment-dwim-2)))
 
   ;; Minor modes in rhtml
+  (require 'rhtml-mode)
   (add-hook 'rhtml-mode-hook
             (lambda ()
               (auto-complete-mode t)
@@ -94,9 +68,9 @@
               (define-key rhtml-mode-map (kbd "C-c C-r") 'rename-sgml-tag)
               (define-key rhtml-mode-map (kbd "C-c C-c") 'whole-line-or-region-comment-dwim-2)))
 
-  ;; indent two spaces
-  (custom-set-variables '(coffee-tab-width 2))
-
+  ;; Awesome plugin for programming Rails App.
+  (require 'rinari)
+  (setq rinari-tags-file-name "TAGS")
   (add-hook 'rinari-minor-mode-hook     ; TODO: rinari-web-server-restart binding to z
             (lambda ()
               (define-key rinari-minor-mode-map (kbd "C-.") 'insert-arrow))))
