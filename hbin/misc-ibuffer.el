3f3f50cb1ed7cc05ec18e46bdc9c76e6698e082e
hbin <embrace.hbin@gmail.com>
2012-08-25 19:18:51 +0800

    keep the ibuffer list up to date.

--- hbin/misc-ibuffer.el
+++ hbin/misc-ibuffer.el
@@ -69,6 +69,7 @@
 
 (add-hook 'ibuffer-mode-hook
           (lambda ()
+            (ibuffer-auto-mode 1)
             (ibuffer-switch-to-saved-filter-groups "default")))
 
 ;; override default find-file in Ibuffer mode
