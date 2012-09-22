bc5bec28b5a9f093270596d00ddf31af79cbb24e
hbin <embrace.hbin@gmail.com>
2012-09-22 22:47:05 +0800

    Autoload lua mode

--- hbin/prog-lua.el
+++ hbin/prog-lua.el
@@ -26,7 +26,9 @@
 
 ;;;###autoload
 (progn
-  (require 'lua-mode)
+  (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+  (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+  (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
   (add-hook 'lua-mode-hook
             (lambda ()
