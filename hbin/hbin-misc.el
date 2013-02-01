b0134dbf00418139b29756d8cdd63418fa37c56d
hbin <embrace.hbin@gmail.com>
2013-02-02 04:45:28 +0800

    FIX: ffap also ignore the C-style comment '/**'

--- hbin/hbin-misc.el
+++ hbin/hbin-misc.el
@@ -71,7 +71,23 @@
 (require 'flymake-cursor)
 
 ;; Ignore '/'
+(require 'ffap)
+(defvar ffap-c-commment-regexp "^/\\*+"
+  "Matches an opening C-style comment, like \"/***\".")
+
+(defadvice ffap-file-at-point (after avoid-c-comments activate)
+  "Don't return paths like \"/******\" unless they actually exist.
+This fixes the bug where ido would try to suggest a C-style
+comment as a filename."
+  (ignore-errors
+    (when (and ad-return-value
+               (string-match-p ffap-c-commment-regexp
+                               ad-return-value)
+               (not (ffap-file-exists-string ad-return-value)))
+      (setq ad-return-value nil))))
+
 (defadvice ffap-file-at-point (after ffap-file-at-point-after-advice ())
+  "ffap ignore the '/'"
   (if (string= ad-return-value "/")
       (setq ad-return-value nil)))
 (ad-activate 'ffap-file-at-point)
