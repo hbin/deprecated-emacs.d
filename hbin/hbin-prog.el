ccc4aa9da34994ea0aa2ca0e5df1b4ea53812e36
hbin <embrace.hbin@gmail.com>
2013-02-18 10:06:58 +0800

    Enable auto-filling for comments.

--- hbin/hbin-prog.el
+++ hbin/hbin-prog.el
@@ -31,8 +31,13 @@
   (textmate-mode 1)
   (rainbow-delimiters-mode 1)
 
+  ;; Enable auto-filling for comments in the current buffer.
+  (set (make-local-variable 'comment-auto-fill-only-comments) t)
+  (auto-fill-mode 1)
+
   (local-set-key (kbd "C-M-h") 'backward-kill-word)
-  (local-set-key (kbd "C-j") 'reindent-then-newline-and-indent))
+  (local-set-key (kbd "C-j") 'reindent-then-newline-and-indent)
+  (local-set-key (kbd "C-c C-c") 'whole-line-or-region-comment-dwim-2))
 
 (add-hook 'prog-mode-hook 'hbin-prog-mode-init)
 
