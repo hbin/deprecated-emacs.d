8c70c557a700a47327addf4534f2cade22e18305
hbin <embrace.hbin@gmail.com>
2013-06-25 17:13:00 +0800

    Upped deps, and remove dirtree mode

--- hbin/misc-dired.el
+++ hbin/misc-dired.el
@@ -63,6 +63,13 @@
 (eval-after-load "dired" '(hbin-dired-mode-setup))
 (add-hook 'dired-mode-hook 'hbin-dired-mode-init)
 
+;;; Directory Explorer
+(require 'direx)
+(require 'direx-project)
+(push '(direx:direx-mode :position left :width 40 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx-project:jump-to-project-root-other-window)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'misc-dired)
